623dc12c76aebdb4f0feab3515b2a9c9330f7531
firekicks <114191519+firekicks@users.noreply.github.com>
2024-10-12 13:18:53 -0700

    pushing step 2

--- src/main.ts
+++ src/main.ts
@@ -10,5 +10,19 @@ header.innerHTML = gameName;
 app.append(header);
 
 const button = document.createElement("button");
+button.className = "favorite styled";
+button.type = "button";
 button.innerHTML = "🎉 Let's Play!";
 app.append(button);
+
+let counter: number = 0;
+const unitLabel: string = "celebrations";
+
+const counterDisplay = document.createElement("div");
+counterDisplay.innerHTML = `${counter} ${unitLabel}`;
+app.append(counterDisplay);
+
+button.addEventListener("click", () => {
+  counter++;
+  counterDisplay.innerHTML = `${counter} ${unitLabel}`;
+});
